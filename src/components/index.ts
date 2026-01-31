1c0802ed4b1256a5a772115249fb5739fe0d7e52
Yinuo Zhang <yinuo@aptoslabs.com>
2026-01-31 11:48:32 -0800

    Remove Products export from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Hero } from "./Hero";
 export { default as Mission } from "./Mission";
-export { default as Products } from "./Products";
 export { default as Footer } from "./Footer";
